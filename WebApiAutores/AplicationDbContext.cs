b74b9388b43af827ed98a4fa9f2b8c562fcb73d7
Facu A <kornerito@gmail.com>
2023-01-28 18:38:22 -0300

    first commit

---
+++ WebApiAutores/AplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutoresDb
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+}
---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entidades;
+using WebApiAutoresDb;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/autores")]
+    public class AutoresController: ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Autor>>> Get()
+        {
+            return await context.Autores.ToListAsync();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Post(Autor autor)
+        {
+            context.Add(autor);
+            await context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
